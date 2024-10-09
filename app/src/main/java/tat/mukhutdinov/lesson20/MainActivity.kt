95c186923827bf80d9089aeb6e3d1a726c5bfa91
Bulat <bulatmm@yandex.ru>
2024-10-09 16:30:21 +0500

    Initial commit

---
+++ app/src/main/java/tat/mukhutdinov/lesson20/MainActivity.kt
@@ -0,0 +1,18 @@
+package tat.mukhutdinov.lesson20
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import tat.mukhutdinov.lesson20.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+}
